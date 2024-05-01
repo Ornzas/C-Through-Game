48d873c8f8d7b96def0868ab8d78d0034890345d
Dmitri Kasatkin <kasatkin.obit@gmail.com>
2024-05-01 17:26:05 +0300

    add body new simple programm

--- counter.cpp
+++ counter.cpp
@@ -1,3 +1,11 @@
 /*
 Программа из второй главы, попытка добавить в репозиторий прошла успешно
 */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t = 10;
+    cout << t << endl;
+}
