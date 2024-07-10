193c8fefcb464a9aa60c5d8e454e53076324db04
Dmitri Kasatkin <kasatkin.obit@gmail.com>
2024-07-10 12:41:20 +0300

    add tik-tac-tor application

---
+++ section3/tik-tac-toe_board.cpp
@@ -0,0 +1,17 @@
+// Программа Tic-Tac-Toe Board
+// Демонстрирует работу с многомерными массивами
+#include <iostream>
+using namespace std;
+int main()
+{
+    const int ROWS = 3;
+    const int COLUMNS = 3;
+    char board[ROWS][COLUMNS] = { {'O', 'X', 'O'},
+                                  {' ', 'X', 'X'},
+                                  {'X', 'O', 'O'}  };
+    cout << "Here's the tic-tac-toe board:\n";
+    for (int i = 0; i < ROWS; ++i)
+    {
+        
+    }
+}
